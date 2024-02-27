25efe7f8ba87b45974426c1886170a6b85587d45
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-26 21:18:06 -0300

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-      <li><a>Inicio</a></li>
-      <li><a>Contacto</a></li>
-      <li><a>Curriculum Vitae</a></li>
+      <li><Link to="/">Inicio</Link></li>
+    <li><Link to="/ContactMe">Contacto</Link></li> 
+    <li><Link to="https://drive.google.com/file/d/1NTbY_1BLLZz8GAv7jiNs3qo1VqXawtd2/view?usp=sharing" target="_blank" >Curriculum</Link></li>
 
       </ul>
     </div>
