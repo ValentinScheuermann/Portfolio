e8332edabbb32397ad4da03440764c64eff4e9f0
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-02-28 19:51:35 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <BrowserRouter>
+        <SpeedInsights />
          <App />
         </BrowserRouter>
   </React.StrictMode>,
