37bdea0256b11c4eb11c05c3f9ac509ad50ce933
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-03-01 22:47:14 -0300

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import logomio2 from '../../assets/logomio2.png'
 const Navbar = () => {
   return (
 <div className="navbar">
-  <div className="navbar-start">
+  <div className="navbar-center">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -26,7 +26,7 @@ const Navbar = () => {
     </a>
     <a href='/' className="btn btn-ghost text-xl">Portfolio</a>
   </div>
-  <div className="navbar-start hidden lg:flex">
+  <div className="navbar-end hidden lg:flex">
   <ul className="menu menu-horizontal px-0">
     <li><Link to="/">Inicio</Link></li>
     <li><Link to="/ContactMe">Contacto</Link></li> 
