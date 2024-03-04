702fe2b17d6a75ad4edd25c310a7168f138054fd
ValentinScheuermann <valen.scheuermann1@gmail.com>
2024-03-04 15:36:58 -0300

    .

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -9,7 +9,7 @@ const Contacto = () => {
   return (
     <div className="relative pt-40 pb-20 lg:pt-44">
       <div className="relative xl:container m-auto px-6 md:px-12 lg:px-6">
-        <h1 className="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-4xl text-center sm:text-5xl md:text-6xl lg:w-auto lg:text-left xl:text-7xl dark:text-white">¿Queres trabajar conmigo? <br className="lg:block hidden" /> 
+        <h1 className="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-4xl text-center sm:text-5xl md:text-6xl lg:w-auto lg:text-left xl:text-7xl dark:text-blue">¿Queres trabajar conmigo? <br className="lg:block hidden" /> 
         <span className="relative text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary dark:from-primaryLight dark:to-secondaryLight">Aca abajo te dejo mi contacto y Curriculum</span></h1>
         <div className="lg:flex">
           <div className="relative mt-8 md:mt-16 space-y-8 sm:w-10/12 md:w-2/3 lg:ml-0 sm:mx-auto text-center lg:text-left lg:mr-auto lg:w-7/12">
--- src/components/Propuesta/Propuesta.jsx
+++ src/components/Propuesta/Propuesta.jsx
@@ -7,7 +7,7 @@ const Propuesta = () => {
   return (
     <div>
         <h1 className="sm:mx-auto sm:w-10/12 md:w-2/3 font-black text-blue-900 text-4xl text-center sm:text-5xl md:text-6xl
-         lg:w-auto lg:text-left xl:text-7xl dark:text-white">¿Te gustaría un Portfolio como este? <br className="lg:block hidden" /> 
+         lg:w-auto lg:text-left xl:text-7xl dark:text-blue">¿Te gustaría un Portfolio como este? <br className="lg:block hidden" /> 
         <span className="relative text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary 
         dark:from-primaryLight dark:to-secondaryLight">¿Tener tu propia Landing Page? ¡Contactame y hablemos!</span></h1>
     <div className="lg:flex">
